4b47a5d7955a2d877e7a02e5c98c2b6891420daf
frappaaa <francescoluppi@aol.com>
2021-04-08 21:16:35 +0200

    Add: radio buttons

--- src/components/stepForm/Step1.js
+++ src/components/stepForm/Step1.js
@@ -3,16 +3,29 @@ import React from "react";
 function Step1(props) {
   return (
     <div>
-      <p>
+      <p className="text-center">
         Hai un tetto di almeno 2.500m2 ad uso non residenziale?
-        <input
-          type="radio"
-          name="tetto"
-          value={props.getState("tetto", "")}
-          onChange={props.handleChange}
-        />
-        <label for="tetto">Si</label>
       </p>
+      <div className="text-center">
+        <label for="si">
+          <input
+            type="radio"
+            name="si"
+            value={props.getState("si", "")}
+            onChange={props.handleChange}
+          />
+          Si
+        </label>
+        <label for="no">
+          <input
+            type="radio"
+            name="no"
+            value={props.getState("no", "")}
+            onChange={props.handleChange}
+          />
+          No
+        </label>
+      </div>
     </div>
   );
 }
--- src/components/stepForm/Step3.js
+++ src/components/stepForm/Step3.js
@@ -3,6 +3,7 @@ import React from "react";
 function Step3(props) {
   return (
     <div>
+      <p>Tetto: {props.state.si}</p>
       <p>Name: {props.state.name}</p>
       <p>Surname: {props.state.surname}</p>
       <p>Email: {props.state.email}</p>
