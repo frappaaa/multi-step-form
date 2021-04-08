53f945287dc838b143cc9f9b1f6be71ae6ab51d5
frappaaa <francescoluppi@aol.com>
2021-04-08 23:50:38 +0200

    DEL: tutti i file inutili del form vecchio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Form from "./components/Form";
-
 export default function App() {
   return (
     <div className="App max-w-screen-md m-auto p-6 rounded-md mt-10 shadow-md">
       <h1 className="text-center">Multi Step Form</h1>
-      <Form />
     </div>
   );
 }
