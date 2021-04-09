ca45a817e7e612e4ee9a53a11e6b3fd2c7f0457a
frappaaa <francescoluppi@aol.com>
2021-04-09 18:19:43 +0200

    FIX: rimosso mapbox e aggiunto google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,68 +1,46 @@
-import React, { useRef, useEffect } from "react";
+import React, { Component } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-//MapBox
-import mapboxgl from "mapbox-gl";
-
-//Draw
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
-import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-
-//geocoder
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZnJhbmNlc2NvbHVwcGkiLCJhIjoiY2tuMzBhd3pyMTFxajJ1bHJxajV6bTdnZiJ9.dO_oLJys8zuPkTqbf5zTlA";
-
-const Map = () => {
-  const mapContainerRef = useRef(null);
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [12.489, 41.915],
-      zoom: 4.5,
-    });
-
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      }),
-      "top-right"
-    );
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), "top-right");
-
-    const Draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-        polygon: true,
-        trash: true,
-      },
-    });
-    const Geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-    });
-
-    // Map#addControl takes an optional second argument to set the position of the control.
-    // If no position is specified the control defaults to `top-right`. See the docs
-    // for more details: https://docs.mapbox.com/mapbox-gl-js/api/#map#addcontrol
-
-    map.addControl(Geocoder, "top-left");
-    map.addControl(Draw, "top-left");
-
-    // Clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  return (
-    <div style={{ width: "100%", height: "400px" }} ref={mapContainerRef} />
-  );
+const containerStyle = {
+  width: "100%",
+  height: "400px",
 };
 
-export default Map;
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+export default function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDjBazoygjwpNABxbo-UDJpbg1SCed9X8U",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
--- src/components/steps/Step2.js
+++ src/components/steps/Step2.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Map from "../Map";
+import MapContainer from "../Map";
 
 const Step1 = () => {
   return (
     <div id="step2" className="mt-10 rounded-md shadow-md border-2">
-      <Map />
+      <MapContainer />
     </div>
   );
 };
