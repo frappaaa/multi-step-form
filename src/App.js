5cd08aea968f6ce16dd3d35e7727dacafd0ffdeb
frappaaa <francescoluppi@aol.com>
2021-04-09 11:02:27 +0200

    ADD: formik and yup and form starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import MultiStepForm from "./components/MultiStepForm";
+
 export default function App() {
   return (
     <div className="App max-w-screen-md m-auto p-6 rounded-md mt-10 shadow-md">
       <h1 className="text-center">Multi Step Form</h1>
+      <MultiStepForm />
     </div>
   );
 }
