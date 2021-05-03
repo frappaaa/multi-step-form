6ee53256384c7bcd0496d40df8323a201956def0
frappaaa <francescoluppi@aol.com>
2021-05-03 16:57:40 +0200

    FIX: cambiato il colore dell'animazione

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,26 +89,26 @@ prevBtn.addEventListener("click", () => {
   progress((100 / stepCount) * current_step);
 });
 
-submitBtn.addEventListener("click", () => {
-  preloader.classList.add("d-block");
+// submitBtn.addEventListener("click", () => {
+//   preloader.classList.add("d-block");
 
-  const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+//   const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-  timer(3000)
-    .then(() => {
-      bodyElement.classList.add("loaded");
-    })
-    .then(() => {
-      step[stepCount].classList.remove("d-block");
-      step[stepCount].classList.add("d-none");
-      prevBtn.classList.remove("d-inline-block");
-      prevBtn.classList.add("d-none");
-      submitBtn.classList.remove("d-inline-block");
-      submitBtn.classList.add("d-none");
-      succcessDiv.classList.remove("d-none");
-      succcessDiv.classList.add("d-block");
-    });
-});
+//   timer(3000)
+//     .then(() => {
+//       bodyElement.classList.add("loaded");
+//     })
+//     .then(() => {
+//       step[stepCount].classList.remove("d-block");
+//       step[stepCount].classList.add("d-none");
+//       prevBtn.classList.remove("d-inline-block");
+//       prevBtn.classList.add("d-none");
+//       submitBtn.classList.remove("d-inline-block");
+//       submitBtn.classList.add("d-none");
+//       succcessDiv.classList.remove("d-none");
+//       succcessDiv.classList.add("d-block");
+//     });
+// });
 
 /*Controllo domanda 5 */
 
